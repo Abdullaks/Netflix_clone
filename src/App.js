9108d20131c410a99f5e93fffb07b36f74fcaf8a
Abdullaks <abdullaks1999@gmail.com>
2022-08-24 19:03:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import React from 'react'
+import NavBar from "./Components/NavBar/NavBar";
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+import{action,originals} from './urls'
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+      <RowPost  url={originals} title='Netflix Originals'   />
+      <RowPost  url={action}  title='Action Movies'  isSmall/>
+
+      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Constants/Constants.js
@@ -0,0 +1,4 @@
+
+export const baseUrl='https://api.themoviedb.org/3'
+export const API_KEY = "ac1926ea408cfc01916fb528f786a894"
+export const imageUrl='https://image.tmdb.org/t/p/original'
